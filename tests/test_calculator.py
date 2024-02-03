a136d60942b5e648d1dc5e64adf307c1cc3d8abb
russf19 <russ_f19@verizon.net>
2024-02-03 17:01:25 -0500

    calculator changes

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,11 @@
-from calculator import add
+'''Russell's Calculator Test'''
+from calculator import add, subtract
+
 
 def test_addition():
+    '''Test that addition function works'''
     assert add(2,2) == 4
+
+def test_subtraction():
+    '''Test that subtraction function works'''
+    assert subtract(2,2) == 0
